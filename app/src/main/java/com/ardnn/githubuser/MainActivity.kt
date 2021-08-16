a58b1a078c397ea3add5e3abbca586bc71b97f3f
muammarahlnn <21muammarahlan@gmail.com>
2021-08-16 10:10:28 +0800

    add toolbar in main activity

--- app/src/main/java/com/ardnn/githubuser/MainActivity.kt
+++ app/src/main/java/com/ardnn/githubuser/MainActivity.kt
@@ -2,10 +2,28 @@ package com.ardnn.githubuser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.ardnn.githubuser.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // view binding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        // set action bar
+        setSupportActionBar(binding.toolbarMain)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar_main, menu)
+        return super.onCreateOptionsMenu(menu)
+
     }
 }
